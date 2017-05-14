1fa63e5c83fb3ba6ff876dc86ab7100c5bbb77c6
Pawan Dubey <pawanthegunner@gmail.com>
2017-05-14 17:49:41 +0530

    Add tests from https://github.com/jakedouglas/fnv-ruby

--- test/fnv/hash_test.rb
+++ test/fnv/hash_test.rb
@@ -5,7 +5,23 @@ class Fnv::HashTest < Minitest::Test
     refute_nil ::Fnv::Hash::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_fnv_1
+    fnv_1_32_data.each do |data, hash_expected|
+      assert_equal Fnv::Hash.fnv_1(data, size: 32), hash_expected
+    end
+
+    fnv_1_64_data.each do |data, hash_expected|
+      assert_equal Fnv::Hash.fnv_1(data, size: 64), hash_expected
+    end
+  end
+
+  def test_fnv_1a
+    fnv_1a_32_data.each do |data, hash_expected|
+      assert_equal Fnv::Hash.fnv_1a(data, size: 32), hash_expected
+    end
+
+    fnv_1a_64_data.each do |data, hash_expected|
+      assert_equal Fnv::Hash.fnv_1a(data, size: 64), hash_expected
+    end
   end
 end
